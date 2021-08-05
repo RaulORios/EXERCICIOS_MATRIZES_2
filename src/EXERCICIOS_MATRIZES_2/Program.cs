b898b482acb23aba0e9e28da20263e2ba45e4189
RaulORios <raul.orios89@outlook.com>
2021-08-05 12:57:25 -0300

    Program.cs adicionado

---
+++ src/EXERCICIOS_MATRIZES_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EXERCICIOS_MATRIZES_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+            int[,] a;
+
+            string[] s = Console.ReadLine().Split(' ');
+            m = int.Parse(s[0]);
+            n = int.Parse(s[1]);
+            a = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            int[] vet = new int[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                int soma = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    soma = soma + a[i, j];
+                }
+                vet[i] = soma;
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
